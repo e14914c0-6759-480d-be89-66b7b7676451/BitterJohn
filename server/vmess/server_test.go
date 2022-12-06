bc42f5b2bfc99cb70cc38897264a5b1d5d08696a
mzz2017 <mzz@tuta.io>
2022-12-06 17:30:49 +0800

    test: add test for vmess+tls+grpc

--- server/vmess/server_test.go
+++ server/vmess/server_test.go
@@ -2,10 +2,15 @@ package vmess
 
 import (
 	"context"
-	"github.com/mzz2017/softwind/protocol/vmess"
+	"github.com/e14914c0-6759-480d-be89-66b7b7676451/BitterJohn/pkg/log"
 	"github.com/e14914c0-6759-480d-be89-66b7b7676451/BitterJohn/server"
 	"github.com/e14914c0-6759-480d-be89-66b7b7676451/SweetLisa/model"
+	proto "github.com/mzz2017/softwind/pkg/gun_proto"
+	"github.com/mzz2017/softwind/protocol/vmess"
+	grpc2 "github.com/mzz2017/softwind/transport/grpc"
 	"golang.org/x/net/proxy"
+	"google.golang.org/grpc"
+	"net"
 	"testing"
 )
 
@@ -30,7 +35,49 @@ func TestServer(t *testing.T) {
 	}}); err != nil {
 		t.Fatal(err)
 	}
-	if err := svr.Listen("localhost:18080"); err != nil {
+	t.Log("Listen at localhost:18080")
+	if err := svr.Listen("tcp://localhost:18080"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGrpcServer(t *testing.T) {
+	log.SetLogLevel("trace")
+	doubleCuckoo := vmess.NewReplayFilter(120)
+	svr, err := New(context.WithValue(context.Background(), "doubleCuckoo", doubleCuckoo), proxy.Direct)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err = svr.AddPassages([]server.Passage{{
+		Manager: false,
+		Passage: model.Passage{
+			In: model.In{
+				From: "",
+				Argument: model.Argument{
+					Protocol: "vmess",
+					Password: "28446de9-2a7e-4fab-827b-6df93e46f945",
+				},
+			},
+			Out: nil,
+		},
+	}}); err != nil {
+		t.Fatal(err)
+	}
+
+	lt, err := net.Listen("tcp", "localhost:18443")
+	if err != nil {
+		t.Fatal(err)
+	}
+	s := svr.(*Server)
+	s.grpc = grpc2.Server{
+		Server:     grpc.NewServer(),
+		LocalAddr:  lt.Addr(),
+		HandleConn: s.handleConn,
+	}
+	proto.RegisterGunServiceServerX(s.grpc.Server, s.grpc, "GunService")
+
+	t.Log("Serve at https://localhost:18443")
+	if err = s.grpc.Serve(lt); err != nil {
 		t.Fatal(err)
 	}
 }
